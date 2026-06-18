277bd42e984657ba39c2cfb8824dfed859590aaa
daviskiplangat <daviskiplangat74@gmail.com>
2026-06-18 15:01:08 +0300

    learnt string interpolation

---
+++ intro/tempelate string.js
@@ -0,0 +1,13 @@
+let firstName="dave"
+let secondName="singoei"
+let age=30
+let address="nairobi"
+
+let userDetails=firstName+" "+secondName+" is "+age+" years old and lives in "+address
+console.log(userDetails)
+console.lobg("user details is",typeof userDetails)
+
+//tempelate string use backticks
+let userDetails2=`${firstName} ${secondName} is ${age} years old and lives in ${address}`
+console.log(userDetails2)
+console.log(`${firstName} ${secondName} is ${age} years old and lives in ${address}`)
