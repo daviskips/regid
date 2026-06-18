e19e62a798d4b071cb5ef806f8f310c312b86b3c
daviskiplangat <daviskiplangat74@gmail.com>
2026-06-18 17:02:57 +0300

    Add mathoperator.js and update index.html

---
+++ intro/mathoperator.js
@@ -0,0 +1,36 @@
+/*
+Assigment=
+Addition + ->use to add two or more numbers
+multiplication* ->use to multiply two or more numbers
+-->powes
+*/
+let numb1=23;//Declared and assigned the variable
+console.log(`numb1 is ${numb1}its its typeis ${typeof numb1}`)
+numb1=40;//Assigment only
+console.log(`numb1 is ${numb1}its its typeis ${typeof numb1}`)
+let numb2=60
+console.log(`numb2 is ${numb2}its its typeis ${typeof numb2}`)
+letnumb3=numb1+numb2+10;//40+60+10 =>110
+console.log(`numb3 is ${numb3}its its typeis ${typeof numb3}`)
+let numb4=numb1*numb2
+console.log(`numb4 is ${numb4}its its typeis ${typeof numb4}`)
+let numb5=numb1**3//2*2*2 power ->2*2*2
+console.log(`numb5 is ${numb5}its its typeis ${typeof numb5}`)
+let numb6=math.pow(10,3)//2^3
+console.log(`numb6 is ${numb6}its its typeis ${typeof numb6}`)
+
+//
+let numb7=30
+let numb8=10
+let divnumb=numb7/numb8
+console.log(`divnumb is ${divnumb}its its typeis ${typeof divnumb}`)
+
+//modulus is somehow special
+//divide2 numbers ->remainder
+let numb9=23
+let number10=12
+let modnumb=numb9%number10
+console.log(`modnumb is ${modnumb}its its typeis ${typeof modnumb}`)
+
+//calculation ->
+//is a number x a multiple of 11
