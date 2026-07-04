35225369e29b87857273f09e34bc0b42c352c33b
daviskiplangat <daviskiplangat74@gmail.com>
2026-07-04 21:43:36 +0300

    callback

---
+++ junior-dev/mycallbackfunction.js
@@ -0,0 +1,36 @@
+/*
+->our callback function
+->doing shape stuff
+---->shapes,
+circle ->
+rectangle ->
+triangle ->
+
+1.for each of the shapes
+create a function<arrow,name>
+to calculate the area. ensure
+the function takes required parameters.
+test the function
+2.we are create a super function called shape.
+its going to take the following parameters
+@param1=>function<correct shape>required
+@param2=>shapename<string>
+@param3=>s1<required>
+@param4=>s2<optional>
+@param5=>s3<optional>
+1.inside your functionshape.
+console each of the parameters and there types.
+2.for each of your functi0n ie for areacircle,
+arearectangle,areatriangle
+callthe shape function and pass the required parameters.
+example for areacircle
+*/
+circle area:math.PI * r * r; //area of circle
+const calculateCircleArea = (r) => math.
+
+rectangle area = length * width; //area of rectangle
+const calculateRectangleArea = (length, width) => length * width;
+
+triangle area = 0.5 * base * height; //area of triangle
+const calculateTriangleArea = (base, height) => 0.5 * base * height;
+
